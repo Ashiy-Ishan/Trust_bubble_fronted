868690171869049cce33817fcfe1d6d2de9f9b87
Semini Herath <semiwasanaherath@gmail.com>
2025-09-09 17:21:35 +0530

    update 3

--- app/src/main/java/com/example/trust_bubble/network/RetrofitClient.kt
+++ app/src/main/java/com/example/trust_bubble/network/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 object RetrofitClient {
     // IMPORTANT: REPLACE WITH YOUR IP ADDRESS OR NGROK URL
-    private const val BASE_URL = "https://ccdf4596fbe2.ngrok-free.app/"
+    private const val BASE_URL = "https://d8fdf229be36.ngrok-free.app/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -15,8 +15,8 @@ object RetrofitClient {
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(loggingInterceptor)
         .connectTimeout(30, TimeUnit.SECONDS) // Wait 60 seconds to connect
-        .readTimeout(30, TimeUnit.SECONDS)    // Wait 60 seconds for a response
-        .writeTimeout(10, TimeUnit.SECONDS)   // Wait 60 seconds to send data
+        .readTimeout(60, TimeUnit.SECONDS)    // Wait 60 seconds for a response
+        .writeTimeout(10, TimeUnit.SECONDS)   // Wait 10 seconds to send data
         .build()
 
 
